28573f754700466b270c02e6947cd6ff481da5ae
Diana <diana.menk@mail.ru>
2026-04-16 16:12:08 +0700

    Сделано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+'use strict'
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = (b ** 2) - 4 * a * c;
+
+  if (D > 0) {
+    arr[0] = (-b + Math.sqrt(D)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(D)) / (2 * a);
+  } else if (D === 0) {
+    arr[0] = -b / (2 * a);
+  } else {
+    console.log('Уравнение не имеет корней');
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let P = percent / 100 / 12;
+	let S = amount - contribution;
+	let monthlyPayment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+	let Sum = countMonths * monthlyPayment;
   
+	return Sum.toFixed(2);
 }
