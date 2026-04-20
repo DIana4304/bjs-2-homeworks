7e3cb1b208712956fff4f7ac974459a1d54069a5
Diana <diana.menk@mail.ru>
2026-04-20 16:40:32 +0700

    Исправлено первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,26 @@
 'use strict'
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D = (b ** 2) - 4 * a * c;
+	let arr = [];
+	let Discriminant = (b ** 2) - 4 * a * c;
 
-  if (D > 0) {
-    arr[0] = (-b + Math.sqrt(D)) / (2 * a);
-    arr[1] = (-b - Math.sqrt(D)) / (2 * a);
-  } else if (D === 0) {
-    arr[0] = -b / (2 * a);
-  } else {
-    console.log('Уравнение не имеет корней');
-  }
-
-  return arr;
+	if (Discriminant > 0) {
+		arr[0] = (-b + Math.sqrt(Discriminant)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(Discriminant)) / (2 * a);
+	} else if (Discriminant === 0) {
+		arr[0] = -b / (2 * a);
+	} else {
+		console.log('Уравнение не имеет корней');
+	}
+	
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent / 100 / 12;
-	let S = amount - contribution;
-	let monthlyPayment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+	let percentMonth = percent / 100 / 12;
+	let sum = amount - contribution;
+	let monthlyPayment = sum * (percentMonth + (percentMonth / (((1 + percentMonth)**countMonths) - 1)));
 	let Sum = countMonths * monthlyPayment;
-  
-	return Sum.toFixed(2);
+	
+	return +Sum.toFixed(2);
 }
